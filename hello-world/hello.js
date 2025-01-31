76311e9ab3156c8697bc4a7e4996a4d9c1b3eff2
kur <rhdnfka94@gmail.com>
2025-01-31 14:14:23 +0900

    Change fn to response Hello <name>

--- hello-world/hello.js
+++ hello-world/hello.js
@@ -9,6 +9,10 @@ module.exports.handler = async (event) => {
   console.log('Event: ', event);
   let responseMessage = 'Hello, World!';
 
+  if (event.queryStringParameters && event.queryStringParameters['Name']) {
+    responseMessage = 'Hello, ' + event.queryStringParameters['Name'] + '!';
+  }
+
   return {
     statusCode: 200,
     headers: {
@@ -17,5 +21,5 @@ module.exports.handler = async (event) => {
     body: JSON.stringify({
       message: responseMessage,
     }),
-  }
-}
+  };
+};
